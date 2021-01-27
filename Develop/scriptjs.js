ac2460043209816143145f725eeae58d769f2b1b
ligaya <ligaya96@gmail.com>
2021-01-27 00:53:21 -0800

    added some lines to JS/ HTML

--- Develop/scriptjs.js
+++ Develop/scriptjs.js
@@ -1,7 +1,29 @@
-// daily planner
 
-// display time blocks here
+$(document).ready(function(){
+// todays time
+var todays = moment().format("YYYY MMM DDD");
+// hours 
+var hours = moment().format("H")
+var ampm = "";
 
-// save incoming events
+//header time
 
-//
+//rows and colums for
+
+// stored times  
+
+// saved button
+//$(".savedbtn").on("click", function() {
+
+//local drive
+
+
+
+
+
+
+
+
+
+
+});
