7b73ddf7f432426d8afc0837ec361a458e3009b9
ligaya <ligaya96@gmail.com>
2021-01-29 02:22:18 -0800

    redid my HTML

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,13 @@ $(document).ready(function () {
     // todays time
     var today = moment().format("YYYY MMM DDD");
     // hours 
-    var hours = moment().format("H")
+    var Hours24 = moment().format("H");
+    var Hours12 = moment().format("h");
     var ampm = "";
     var displayHour = 0;
-
+    var Hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
     //header time
-    var $todayHeader = $("currentDay");
+    var $todayHeader = $("#currentDay");
     $todayHeader.text(today);
 
     //storing the todos 
@@ -19,58 +20,42 @@ $(document).ready(function () {
         Usertext = newArray(9);
     }
 
-    // //calling into planner-container
-    // var $scheduleDiv = $("planner-container");
-    // $scheduleDiv.empty();
-    // //calling hours
-    // for (var hour = 9; hour <= 17; hour ++) {
-    // var index = hour - 9; 
+    // calling into planner-container
+    var $scheduleDiv = $("planner-container");
+    $scheduleDiv.empty();
+    //calling hours
+   for (var hour = 9; hour <= 17; hour++) {
+        var index = hour - 9;
 
-    //  //rows and colums to the HTML
-    //  var $rowDiv = $("<div>");
-    //  $rowDiv.addclass("row");
-    //  $rowDiv.addclass("daterow");
-    //   $rowDiv.attr("hour-index",hour);
-          
-    //  var $colDiv = $("<div>");
-    //  $colDiv.addclass("col-md-2");    
-
-    //   var $timeElementSpn = $("<span>");
-    //   $timeElementSpn.attr("class","timeElement");
-    //     // display the time
-    //  if (hour > 12) {
-    //     displayHour = hour - 12;
-    //     ampm = "pm";
-    // } else {
-    //     displayHour=hour;
-    //     ampm="am";
-    // }
-    // }
-    //  $timeElementSpn.text("${displayhour} ${ampm}");
-    //  $rowDiv.append($colDiv);
-    //  $colDiv.append($timeElementSpn);
+        var $timeElementSpn = $("<span>");
+        $timeElementSpn.attr("class", "timeElement");
+        // display the time
+        if (hour > 12) {
+            displayHour = hour - 12;
+            ampm = "pm";
+        } else {
+            displayHour = hour;
+            ampm = "am";
+        }
+        $timeElementSpn.text("${displayhour} ${ampm}")
+    }
 
     //building the rows
-   //var 
+    var $dailyplanspn = $("<input>");
+    $dailyplanspn.attr("id,inpute")
+
     // stored times  
 
     // saved button
-    $(".savedbtn").on("click", function(e) {
-        console.log("text")
-        var savedbtn =$(this)
+    $(".savedbtn").on("click", function (e) {
+        //console.log("text")
+        var savedbtn = $(this)
         var key = savedbtn.parent().attr("id")
-        var value= savedbtn.parent().children("text")
-        localStorage.setItem(key,value)
-        console.log(key)
+        var value = savedbtn.parent().children("text")
+        localStorage.setItem(key, value)
+        //console.log(key)
     })
     //local drive
-
-
-
-
-
-
-
 
 
 
