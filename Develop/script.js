5daec8d5b027a1fbcc37d710384a7e7a219060fe
ligaya <ligaya96@gmail.com>
2021-01-29 00:10:26 -0800

    svedBtn works

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     // hours 
     var hours = moment().format("H")
     var ampm = "";
+    var displayHour = 0;
 
     //header time
     var $todayHeader = $("currentDay");
@@ -18,15 +19,49 @@ $(document).ready(function () {
         Usertext = newArray(9);
     }
 
-    //rows and colums to the HTML
-    // var $scheduleDiv = $("planner-container")
-    //$scheduleDiv.empty();
+    // //calling into planner-container
+    // var $scheduleDiv = $("planner-container");
+    // $scheduleDiv.empty();
+    // //calling hours
+    // for (var hour = 9; hour <= 17; hour ++) {
+    // var index = hour - 9; 
 
+    //  //rows and colums to the HTML
+    //  var $rowDiv = $("<div>");
+    //  $rowDiv.addclass("row");
+    //  $rowDiv.addclass("daterow");
+    //   $rowDiv.attr("hour-index",hour);
+          
+    //  var $colDiv = $("<div>");
+    //  $colDiv.addclass("col-md-2");    
+
+    //   var $timeElementSpn = $("<span>");
+    //   $timeElementSpn.attr("class","timeElement");
+    //     // display the time
+    //  if (hour > 12) {
+    //     displayHour = hour - 12;
+    //     ampm = "pm";
+    // } else {
+    //     displayHour=hour;
+    //     ampm="am";
+    // }
+    // }
+    //  $timeElementSpn.text("${displayhour} ${ampm}");
+    //  $rowDiv.append($colDiv);
+    //  $colDiv.append($timeElementSpn);
+
+    //building the rows
+   //var 
     // stored times  
 
     // saved button
-    $(".savedbtn").on("click", function () {
+    $(".savedbtn").on("click", function(e) {
         console.log("text")
+        var savedbtn =$(this)
+        var key = savedbtn.parent().attr("id")
+        var value= savedbtn.parent().children("text")
+        localStorage.setItem(key,value)
+        console.log(key)
     })
     //local drive
 
